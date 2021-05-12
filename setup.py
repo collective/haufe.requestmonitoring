c0be060c33cfba0b7fb7b37764ffb2b5d4430dee
Gil Forcada <gil.gnome@gmail.com>
2021-05-12 15:50:41 +0200

    chore: update trove classifiers

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(
     classifiers=[
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
+        "Framework :: Plone :: 5.2",
         "Development Status :: 5 - Production/Stable",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
@@ -37,6 +38,7 @@ setup(
         "Programming Language :: Python :: 2.5",
         "Programming Language :: Python :: 2.6",
         "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
         "Framework :: Zope2",
         "Topic :: System :: Monitoring",
         "Topic :: System :: Logging",
