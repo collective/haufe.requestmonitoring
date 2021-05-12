ecb93312d705e285b6ec1e9e931fbe21087a9211
Gil Forcada <gil.gnome@gmail.com>
2021-05-12 15:53:07 +0200

    chore: add ZPL trove classifier

--- setup.py
+++ setup.py
@@ -28,10 +28,11 @@ setup(
     # Get more strings from
     # http://www.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
+        "Development Status :: 5 - Production/Stable",
         "Framework :: Plone",
         "Framework :: Plone :: 4.3",
         "Framework :: Plone :: 5.2",
-        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: Zope Public License",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 2.4",
